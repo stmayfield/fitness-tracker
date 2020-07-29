08366fc6db71d62b9c07c9c702c3d3c6522f0d1b
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-07-28 18:03:18 -0700

    boilerplate front-end added, express template created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('/public/'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+require('./routes/html-routes.js')(app);
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+
+})
